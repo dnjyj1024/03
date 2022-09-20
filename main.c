1eb1ab62941eb98c9caccac9b141b0e830e2ace3
dnjyj1024 <dnjyj1024@naver.com>
2022-09-20 17:56:16 +0900

    04

--- main.c
+++ main.c
@@ -5,9 +5,7 @@
 
 int main(int argc, char *argv[]) {
 	int a, b;
-	float i;
-	
-	(float)a/b;
+	float i;	
 	
 	printf("분자를 입력하세요 : ");
 	scanf("%f", &a);
@@ -17,5 +15,5 @@ int main(int argc, char *argv[]) {
 	i = (float)a/b;
 	printf("나누기의 결과는 %f\n", i);
 	
-	return 0;
+	return 0;    
 }
